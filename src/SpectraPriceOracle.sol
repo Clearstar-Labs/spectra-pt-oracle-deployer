5434e595eb721edae64a20c1e24a9fc83101cd71
Ulver <ulver@bytemasons.com>
2025-04-22 16:41:23 +0200

    Make another flattened version for verification

--- src/SpectraPriceOracle.sol
+++ src/SpectraPriceOracle.sol
@@ -1,48 +1,35 @@
-// SPDX-License-Identifier: BUSL-1.1
+// SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.22;
 
 import {IDiscountModel} from "./interfaces/IDiscountModel.sol";
 import {IPrincipalToken} from "./interfaces/IPrincipalToken.sol";
-import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 import {IERC20Metadata} from "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
 
-contract SpectraPriceOracle is OwnableUpgradeable {
+contract SpectraPriceOracle is Ownable {
     uint256 private constant SECONDS_PER_YEAR = 365 days;
-    uint256 private UNIT;
-    address public PT;
-    uint256 public maturity;
+    uint256 private immutable UNIT;
+    address public immutable PT;
+    uint256 public immutable maturity;
     address public discountModel; // External discount model
     uint256 public initialImpliedAPY;
     uint8 private underlyingDecimals;
-    uint256 private underlyingUnit;
-
-    uint256[50] private __gap;
 
     event DiscountModelUpdated(address newModel);
 
-    constructor() {
-        _disableInitializers();
-    }
-
-    function initialize(
-        address _pt,
-        address _discountModel,
-        uint256 _initialImpliedAPY,
-        address initOwner
-    ) external initializer {
-        __Ownable_init(initOwner);
+    constructor(address _pt, address _discountModel, uint256 _initialImpliedAPY, address initOwner) Ownable(initOwner) {
         require(_pt != address(0), "zero address");
         PT = _pt;
         address underlying = IPrincipalToken(PT).underlying();
         underlyingDecimals = IERC20Metadata(underlying).decimals();
-        underlyingUnit = 10 ** underlyingDecimals;
         maturity = IPrincipalToken(PT).maturity();
         discountModel = _discountModel;
         initialImpliedAPY = _initialImpliedAPY;
         UNIT = 10 ** IPrincipalToken(PT).decimals();
+
         uint256 timeLeft = ptTimeLeft();
         uint256 futurePTValue = IPrincipalToken(PT).convertToUnderlying(UNIT);
-        require(getPrice(timeLeft, futurePTValue) > 0, "price must be greater than 0");
+        require(getDiscount(timeLeft, futurePTValue) <= futurePTValue, "discount overflow");
     }
 
     function ptTimeLeft() public view returns (uint256) {
@@ -58,13 +45,14 @@ contract SpectraPriceOracle is OwnableUpgradeable {
 
         uint256 futurePTValue = IPrincipalToken(PT).convertToUnderlying(UNIT);
         //Get the discount with the time left
-        uint256 price = getPrice(timeLeft, futurePTValue);
+        uint256 discount = getDiscount(timeLeft, futurePTValue);
 
-        return (0, int256(price), 0, 0, 0);
+        require(discount <= futurePTValue, "discount overflow");
+        return (0, int256(futurePTValue - discount), 0, 0, 0);
     }
 
-    function getPrice(uint256 timeLeft, uint256 futurePTValue) public view returns (uint256) {
-        return IDiscountModel(discountModel).getPrice(initialImpliedAPY, timeLeft, futurePTValue, underlyingUnit);
+    function getDiscount(uint256 timeLeft, uint256 futurePTValue) public view returns (uint256) {
+        return IDiscountModel(discountModel).getDiscount(initialImpliedAPY, timeLeft, futurePTValue);
     }
 
     /// @notice Update the discount model
